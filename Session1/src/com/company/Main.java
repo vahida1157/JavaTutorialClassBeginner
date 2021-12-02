169803be23173cf726fad917f047b941494cb835
vahida1157 <vahidghasemi119@gmail.com>
2021-12-02 18:59:08 +0330

    session 2 end

--- Session1/src/com/company/Main.java
+++ Session1/src/com/company/Main.java
@@ -3,7 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	    // prinit
+	    // print
+        System.out.println("Hello world");
+        System.out.println("1 + 2");
         System.out.println(1 + 2); // 3
         System.out.println("Hello world" + 1); // Hello world1
         System.out.println("Hello world" + " my dears"); // Hello world my dears
@@ -16,6 +18,5 @@ public class Main {
         System.out.println(2 * 3); // 6
         System.out.println(2 * 2 + "A"); // 4A
         System.out.println("A" + 2 * 2); // A4
-
     }
 }
---
+++ Session2/src/com/company/Main.java
@@ -0,0 +1,49 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // section 1
+/*        String firstName = "vahid";
+        String lastName = "Ghasemi";
+        int age = 23;
+        float length = 179.5f;
+        char character = 'V';
+        boolean isAlive = false;
+
+        System.out.println(firstName);
+        System.out.println(age);
+        System.out.println(length);
+        System.out.println(character);
+        System.out.println(isAlive);*/
+
+        //section 2
+/*        int a = 5;
+        int b = 10;
+        int c = a * b;
+        System.out.println(c);*/
+
+        // section 3
+/*        int years = 5;
+        int month = 12;
+        int allMonth = years * month;
+        System.out.println(allMonth);*/
+
+        // section 4 (casting Widening)
+        /*byte x = 100;
+        float f = x;
+        System.out.println(x);
+        System.out.println(f);*/
+
+        // section 5 (casting Narrowing)
+        /*float f = 100.9f;
+        int i = (int) f;
+        System.out.println(i);
+
+        char c = 'A';
+        byte b = (byte) c;
+        System.out.println(b);*/
+
+        
+    }
+}
