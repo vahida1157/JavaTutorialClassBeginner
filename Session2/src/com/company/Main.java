750996f45000e70cfaa0942988fc0dfe3b3ecc3d
vahida1157 <vahidghasemi119@gmail.com>
2021-12-02 19:01:00 +0330

    uncomment code

--- Session2/src/com/company/Main.java
+++ Session2/src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         // section 1
-/*        String firstName = "vahid";
+        String firstName = "vahid";
         String lastName = "Ghasemi";
         int age = 23;
         float length = 179.5f;
@@ -15,35 +15,35 @@ public class Main {
         System.out.println(age);
         System.out.println(length);
         System.out.println(character);
-        System.out.println(isAlive);*/
+        System.out.println(isAlive);
 
         //section 2
-/*        int a = 5;
+        int a = 5;
         int b = 10;
         int c = a * b;
-        System.out.println(c);*/
+        System.out.println(c);
 
         // section 3
-/*        int years = 5;
+        int years = 5;
         int month = 12;
         int allMonth = years * month;
-        System.out.println(allMonth);*/
+        System.out.println(allMonth);
 
         // section 4 (casting Widening)
-        /*byte x = 100;
+        byte x = 100;
         float f = x;
         System.out.println(x);
-        System.out.println(f);*/
+        System.out.println(f);
 
         // section 5 (casting Narrowing)
-        /*float f = 100.9f;
-        int i = (int) f;
+        float f1 = 100.9f;
+        int i = (int) f1;
         System.out.println(i);
 
-        char c = 'A';
-        byte b = (byte) c;
-        System.out.println(b);*/
+        char c1 = 'A';
+        byte b1 = (byte) c1;
+        System.out.println(b1);
 
-        
+
     }
 }
