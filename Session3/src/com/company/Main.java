05879dd86cce7651d0d28de5357ebcc1cff4ee87
vahida1157 <vahidghasemi119@gmail.com>
2021-12-09 23:30:46 +0330

    uncomment code

--- Session3/src/com/company/Main.java
+++ Session3/src/com/company/Main.java
@@ -3,53 +3,52 @@ package com.company;
 public class Main {
     public static void main(String[] args) {
         //************************************ Boolean Opearators *************************************************
-//        boolean a = true;
-//        boolean b = false;
-//        System.out.println(a);
-//        System.out.println(b);
-//
-//        // or operator (|)
-//        System.out.println(true || true); // true
-//        System.out.println(false || true); // true
-//        System.out.println(true || false); // true
-//        System.out.println(false || false); // false
-//
-//        // and operator (&)
-//        System.out.println(true && true); // true
-//        System.out.println(false && true); // false
-//        System.out.println(true && false); // false
-//        System.out.println(false && false); // false
-//
-//        // xor operator (^)
-//        System.out.println(true ^ true); // false
-//        System.out.println(false ^ true); // true
-//        System.out.println(true ^ false); // true
-//        System.out.println(false ^ false); // false
-//
-//        // not operator (!)
-//        System.out.println(!true);
-//        System.out.println(!false);
+        boolean a = true;
+        boolean b = false;
+        System.out.println(a);
+        System.out.println(b);
+
+        // or operator (|)
+        System.out.println(true || true); // true
+        System.out.println(false || true); // true
+        System.out.println(true || false); // true
+        System.out.println(false || false); // false
+
+        // and operator (&)
+        System.out.println(true && true); // true
+        System.out.println(false && true); // false
+        System.out.println(true && false); // false
+        System.out.println(false && false); // false
+
+        // xor operator (^)
+        System.out.println(true ^ true); // false
+        System.out.println(false ^ true); // true
+        System.out.println(true ^ false); // true
+        System.out.println(false ^ false); // false
+
+        // not operator (!)
+        System.out.println(!true);
+        System.out.println(!false);
 
 
         //************************************** String methods ***********************************************
-//        System.out.println("Hello".length());
-//        System.out.println("Hello my dears".indexOf("ea"));
-//        System.out.println("HELLO Vahid".toLowerCase());
-//        System.out.println("HELLO Vahid".toUpperCase());
-//        System.out.println("HELLO " + "Vahid");
-//        System.out.println("HELLO ".concat("Vahid"));
-//        System.out.println("Hello Hello world".replace("Hello","Bye"));
-//        String[] split = "my-name-is-vahid".split("-");
-//        System.out.println(split[2]);
-//        String[] split1 = "hello hello helo".split("ll");
-//        System.out.println(split1[0]);
-//        System.out.println(split1[1]);
-//        System.out.println(split1[2]);
+        System.out.println("Hello".length());
+        System.out.println("Hello my dears".indexOf("ea"));
+        System.out.println("HELLO Vahid".toLowerCase());
+        System.out.println("HELLO Vahid".toUpperCase());
+        System.out.println("HELLO " + "Vahid");
+        System.out.println("HELLO ".concat("Vahid"));
+        System.out.println("Hello Hello world".replace("Hello", "Bye"));
+        String[] split = "my-name-is-vahid".split("-");
+        System.out.println(split[2]);
+        String[] split1 = "hello hello helo".split("ll");
+        System.out.println(split1[0]);
+        System.out.println(split1[1]);
+        System.out.println(split1[2]);
 
-//        System.out.println("Hello \nmy \nfriends \n.");
-//        System.out.println("Hello\tmy\tfriends\t.");
-//        System.out.println("Hello\b world");
-//        System.out.println("hello \\\"world\"");
-
+        System.out.println("Hello \nmy \nfriends \n.");
+        System.out.println("Hello\tmy\tfriends\t.");
+        System.out.println("Hello\b world");
+        System.out.println("hello \\\"world\"");
     }
 }
